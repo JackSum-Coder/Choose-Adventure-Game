2e69c789904583a29f28c1f8b8575a83689f656f
Jack Sommerville <sommerjack7099@gmail.com>
2026-06-17 15:47:12 -0500

    here is my adventure game

---
+++ script.js
@@ -0,0 +1,26 @@
+let nameInput = document.querySelector(".name")
+let image = document.querySelector(".jazzman")
+let button = document.querySelector(".button1")
+let manText = document.querySelector(".manSpeaking")
+let yes = document.querySelector(".yes1")
+let no = document.querySelector(".no1")
+
+yes.style.display = "none"
+no.style.display = "none"
+
+
+//first event name enter 
+button.addEventListener("click", function(){
+    
+    manText.innerHTML = "Nice to meet you " + nameInput.value + ". Alright kid, here's the deal: you need to go visit George's Music Store and buy a keyboard, okay? Remember, you don't got a lotta time so you better hurry up son.";
+    button.style.display = "none";
+    nameInput.style.display = "none";
+    yes.style.display = ""
+    no.style.display = ""
+});
+yes.addEventListener("click", function(){
+
+})
+no.addEventListener("click", function(){
+    manText.innerHTML = "Man...alright. I guess I'll look for someone else...have a nice one bud."
+})
